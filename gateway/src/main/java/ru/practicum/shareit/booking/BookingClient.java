dfa5948c6c219537dadab436760993bf3b7564c1
andreylitvishchenko <mrruiner161@gmail.com>
2025-07-15 13:53:20 +0300

    16 спринт bug fix

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -2,23 +2,20 @@ package ru.practicum.shareit.booking;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-
-
 import ru.practicum.shareit.booking.dto.BookingState;
 import ru.practicum.shareit.client.BaseClient;
+
 
 @Service
 public class BookingClient extends BaseClient {
     private static final String API_PREFIX = "/bookings";
 
-    @Autowired
     public BookingClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
@@ -36,7 +33,6 @@ public class BookingClient extends BaseClient {
         );
         return get("?state={state}&from={from}&size={size}", userId, parameters);
     }
-
 
     public ResponseEntity<Object> bookItem(long userId, BookingDto bookingDto) {
         return post("", userId, bookingDto);
